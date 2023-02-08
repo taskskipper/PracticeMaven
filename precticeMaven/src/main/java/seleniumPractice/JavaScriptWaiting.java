3a774a5b2354b6ff2ee58f612ef92448c59c9ee8
taskskipper <gpremchand933@gmail.com>
2023-02-08 14:59:57 +0530

    added javascript wait

---
+++ precticeMaven/src/main/java/seleniumPractice/JavaScriptWaiting.java
@@ -0,0 +1,38 @@
+package seleniumPractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaScriptWaiting {
+
+ static WebDriver driver;
+	
+	public static void waitForJavascript(int maxWaitMillis, int pollDelimiter) throws InterruptedException {
+	    double startTime = System.currentTimeMillis();
+	    
+	    
+	    while (System.currentTimeMillis() < startTime + maxWaitMillis) {
+	        String prevState = driver.getPageSource();
+	        Thread.sleep(pollDelimiter); // <-- would need to wrap in a try catch
+	        if (prevState.equals(driver.getPageSource())) {
+	            return;
+	        }
+	    }
+	}
+
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.youtube.com/");
+		
+		waitForJavascript(50000, 500);
+		// TODO Auto-generated method stub
+
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
+	}
+
+}
